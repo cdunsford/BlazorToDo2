e45b367f336b7868261e521d57cd89afe9d23343
cduns <cduns@CDNSFRD-SRFCBK>
2021-09-25 08:46:32 -0400

    Add project files.

---
+++ BlazorToDo2/Shared/ToDoItems.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorToDo2.Shared
+{
+    public class ToDoItems
+    {
+        public string Title { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
